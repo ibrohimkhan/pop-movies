61375442d948241823a7edaf3225a7782ec7a9d1
Ibrohim Kholmatov <ibrohimkhan@gmail.com>
2020-01-21 18:25:03 +0500

    increased targetSdkVersion

--- app/src/main/java/com/udacity/popularmovies/common/NetworkHelper.java
+++ app/src/main/java/com/udacity/popularmovies/common/NetworkHelper.java
@@ -2,7 +2,8 @@ package com.udacity.popularmovies.common;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 
 import com.udacity.popularmovies.MovieApplication;
 
@@ -12,7 +13,17 @@ public final class NetworkHelper {
         ConnectivityManager cm =
                 (ConnectivityManager) MovieApplication.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
+        if (cm == null) return false;
+
+        Network network = cm.getActiveNetwork();
+        NetworkCapabilities capabilities = cm.getNetworkCapabilities(network);
+
+        if (capabilities == null) return false;
+
+        if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+            return true;
+
+        return false;
     }
 }
